af6a48ee2b1a4ab662fa76814ab4dfb8f4f79452
ajimae <meeky.ae@gmail.com>
2020-03-18 13:08:00 +0100

    feat(ui):add basic ui navigation functionalities
    - add the search/address bar component
    - add the tab bar component
    - include basic backwards and forwards navigation control functionality
    
    [Finishes #0001]

---
+++ fixtures.js
@@ -0,0 +1,242 @@
+export default [
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/rkvHXu_Il/rkvHXu_Il-1100-700.jpg',
+    title: 'Le Brûloir',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/rkTnGunIx/rkTnGunIx-1100-700.jpg',
+    title: 'Le Petit Brûloir',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/HknxZ9awg/HknxZ9awg-1100-700.jpg',
+    title: 'Oui Mais Non',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/merchants/rJWPQ2mKx/rJWPQ2mKx-1100-700.jpg',
+    title: 'PERKO',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/merchants/rJWPQ2mKx/rJWPQ2mKx-1100-700.jpg',
+    title: 'Perko',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/B1XmNBmLe/B1XmNBmLe-1100-700.jpg',
+    title: 'Café Saint-Henri | Marché Jean-Talon',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/rkvHXu_Il/rkvHXu_Il-1100-700.jpg',
+    title: 'Le Brûloir',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/rkTnGunIx/rkTnGunIx-1100-700.jpg',
+    title: 'Le Petit Brûloir',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/HknxZ9awg/HknxZ9awg-1100-700.jpg',
+    title: 'Oui Mais Non',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/merchants/rJWPQ2mKx/rJWPQ2mKx-1100-700.jpg',
+    title: 'PERKO',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/merchants/rJWPQ2mKx/rJWPQ2mKx-1100-700.jpg',
+    title: 'Perko',
+  },
+  {
+    image: 'https://cdn.th3rdwave.coffee/articles/B1XmNBmLe/B1XmNBmLe-1100-700.jpg',
+    title: 'Café Saint-Henri | Marché Jean-Talon',
+  },
+];
+
+export const html = `<div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  <div style="background:#f0f0f0;margin:0 auto; padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size:12px;font-family:verdana;border-radius:5px;color:#444">
+  <div>
+        <img src=https://res.cloudinary.com/dstvcmycn/image/upload/v1543246158/Author%27s%20Haven/Asset_1_4x.png width="70px" height="70px">
+    </div>
+    <h2>Welcome to Authors Haven, ${'username'}</h2><br>
+    <p>Please click the button below to verify your account.<p>
+  
+  <div style="margin: 50px 50px">
+    <p><a style="background:#e66869;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
+      color:white;text-decoration:none;text-align:center;padding:13px 20px;border-radius:25px;
+      border:0.5px solid #e66869;" 
+          href="${'baseUrl'}/users/verify?id=${'hash'}">Verify your account
+        </a>
+    </p>
+  </div>
+  <p style="margin-top:50px;">
+    If you didn't sign up an account on Authors Haven please ignore this email</p><br>
+    <span style="font-family:sans-serif;font-size:10px">&copy; 2018, Authors Haven</span>
+  </div>
+  `;
---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+export const assets = {
+  'back-off': require('./back-off.png'),
+  'next-off': require('./next-off.png'),
+  plus: require('./plus.png'),
+  tab: require('./tab.png'),
+  more: require('./more.png'),
+};
+
+export const icons = [
+  require('./back.png'),
+  require('./next.png'),
+  require('./plus-0.png'),
+  require('./tab.png'),
+  require('./more-3.png'),
+];
